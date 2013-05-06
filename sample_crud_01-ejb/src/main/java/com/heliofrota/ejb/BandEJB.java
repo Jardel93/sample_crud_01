36aafc922c6532eca09e96a8f82498f441e79a0d
heliofrota <heliofrota@gmail.com>
2013-05-06 10:45:28 -0300

    : ]

--- sample_crud_01-ejb/src/main/java/com/heliofrota/ejb/BandEJB.java
+++ sample_crud_01-ejb/src/main/java/com/heliofrota/ejb/BandEJB.java
@@ -1,6 +1,10 @@
 package com.heliofrota.ejb;
 
+import com.heliofrota.entity.Band;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * EJB class BandEJB.
@@ -8,7 +12,29 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class BandEJB {
+
+    @PersistenceContext()
+    private EntityManager em;
+
+    public void add(Band band) {
+        em.persist(band);
+    }
+
+    public void update(Band band) {
+        em.merge(band);
+    }
+
+    public void delete(Band band) {
+        em.remove(em.find(Band.class, band.getId()));
+    }
     
+    public List<Band> findAll() {
+        return em.createQuery("select b from Band b", Band.class).getResultList();
+    }
     
-    
+    public Band findById(int id) {
+        return em.find(Band.class, id);
+    }
+
 }
+
