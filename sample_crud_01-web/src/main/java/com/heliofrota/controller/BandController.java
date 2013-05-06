e3ce5221b3ed6fd4d22c2a316d5d8f11425534b3
heliofrota <heliofrota@gmail.com>
2013-05-06 10:50:14 -0300

    : ]

--- sample_crud_01-web/src/main/java/com/heliofrota/controller/BandController.java
+++ sample_crud_01-web/src/main/java/com/heliofrota/controller/BandController.java
@@ -1,7 +1,12 @@
 package com.heliofrota.controller;
 
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
 
 /**
  * ManagedBean class BandController.
@@ -11,6 +16,60 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class BandController {
     
+    @EJB
+    private BandEJB bandEJB;
     
+    private Band newBand = new Band();
+    private Band selectedBand = new Band();
+    private List<Band> bands;
+    
+    public BandController() {
+    }
+
+    @PostConstruct
+    public void loadAll() {
+        bands = bandEJB.findAll();
+    }
+
+    public List<Band> getBands() {
+        return bands;
+    }
+
+    public Band getSelectedBand() {
+        return selectedBand;
+    }
+
+    public void setSelectedBand(Band selectedBand) {
+        this.selectedBand = selectedBand;
+    }
+
+    public Band getNewBand() {
+        return newBand;
+    }
+
+    public void setNewBand(Band newBand) {
+        this.newBand = newBand;
+    }
+
+    public void doDeleteBand() {
+        bandEJB.delete(selectedBand);
+        loadAll();
+    }
+
+    public void doUpdateBand() {
+        bandEJB.update(selectedBand);
+        loadAll();
+    }
+
+    public void doCreateBand() {
+        bandEJB.add(newBand);
+        loadAll();
+    }
+
+    public void onRowSelectBand(SelectEvent event) {
+        selectedBand = (Band) event.getObject();
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+        requestContext.addCallbackParam("isSelected", selectedBand.getId() != 0);
+    }
     
 }
